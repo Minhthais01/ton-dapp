5f6ba88dc6229e389e53d19d5750aeaefe2bfccf
Trien0303 <phattrien03032002@gmail.com>
2024-11-21 13:35:21 +0000

    sắp xếp, tìm kiên, footer và lịch sử

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/header/page";
+import Footer from "./components/footer/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <Header></Header>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
